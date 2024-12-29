0081cdbc34e2b204e7b9de7c0b3949e2eb67e3ab
Youssif-Adel <joeadel.business@gmail.com>
2024-12-29 16:56:59 +0200

    12/29/2024

---
+++ Problems/2727 Is Object Empty/script.ts
@@ -0,0 +1,14 @@
+// @ts-ignore
+type JSONValue =
+  | null
+  | boolean
+  | number
+  | string
+  | JSONValue[]
+  | { [key: string]: JSONValue };
+type Obj = Record<string, JSONValue> | JSONValue[];
+
+function isEmpty(obj: Obj): boolean {
+  if (Object.entries(obj).length) return false;
+  return true;
+}
