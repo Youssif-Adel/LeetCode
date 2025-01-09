050107f413b2ee92ac2dc96671a6b1ee50ab2e5d
Youssif-Adel <joeadel.business@gmail.com>
2025-01-09 05:58:19 +0200

    1/9/2025

---
+++ Problems/2185 Counting Words With a Given Prefix/script.ts
@@ -0,0 +1,7 @@
+function prefixCount(words: string[], pref: string): number {
+  return words.reduce(
+    (previousValue, currentValue) =>
+      previousValue + (currentValue.startsWith(pref) ? 1 : 0),
+    0,
+  );
+}
