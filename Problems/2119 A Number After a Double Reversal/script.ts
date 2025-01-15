2788ad78cc3f39fb87b91000612fbf63030bfeb0
Youssif-Adel <joeadel.business@gmail.com>
2025-01-15 11:14:21 +0200

    1/15/2025

---
+++ Problems/2119 A Number After a Double Reversal/script.ts
@@ -0,0 +1,7 @@
+function isSameAfterReversals(num: number): boolean {
+  const stringNum = num.toString();
+
+  if (stringNum.length === 1) return true;
+
+  return stringNum[stringNum.length - 1] !== "0";
+}
