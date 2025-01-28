d2352856642e361ac57101ec838815c01007b9cb
Youssif-Adel <joeadel.business@gmail.com>
2025-01-28 22:16:16 +0200

    1/28/2025

---
+++ Problems/1979 Find Greatest Common Divisor of Array/script.ts
@@ -0,0 +1,18 @@
+// Solved
+
+function findGCD(nums: number[]): number {
+  let min = Infinity,
+    max = 0;
+
+  for (const num of nums) {
+    if (num < min) min = num;
+    if (num > max) max = num;
+  }
+  console.log(min, max);
+
+  for (let i = min; i >= 0; i--) {
+    if (min % i === 0 && max % i === 0) return i;
+  }
+}
+
+console.log(findGCD([2, 5, 6, 9, 10]));
