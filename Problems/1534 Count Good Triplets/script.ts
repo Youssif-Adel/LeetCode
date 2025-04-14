b1cc3e0860341bc86085b330a0655146fa962177
Youssif-Adel <joeadel.business@gmail.com>
2025-04-14 23:22:05 +0200

    4/14/2025

--- Problems/1534 Count Good Triplets/script.ts
+++ Problems/1534 Count Good Triplets/script.ts
@@ -1,23 +1,31 @@
+// Solved
+
 function countGoodTriplets(
   arr: number[],
   a: number,
   b: number,
   c: number,
 ): number {
-  let numberOfTriplets = 0;
+  let numGoodTriplets = 0;
 
   for (let i = 0; i < arr.length - 2; i++) {
-    const numb1 = arr[i];
-    const numb2 = arr[i + 1];
-    const numb3 = arr[i + 2];
+    const num1 = arr[i];
 
-    if (
-      Math.abs(numb1 - numb2) <= a &&
-      Math.abs(numb2 - numb3) <= b &&
-      Math.abs(numb1 - numb3) <= c
-    )
-      numberOfTriplets++;
+    for (let j = i + 1; j < arr.length - 1; j++) {
+      const num2 = arr[j];
+
+      if (Math.abs(num1 - num2) <= a) {
+        for (let k = j + 1; k < arr.length; k++) {
+          const num3 = arr[k];
+
+          if (Math.abs(num2 - num3) <= b && Math.abs(num1 - num3) <= c)
+            numGoodTriplets++;
+        }
+      }
+    }
   }
 
-  return numberOfTriplets;
+  return numGoodTriplets;
 }
+
+console.log(countGoodTriplets([3, 0, 1, 1, 9, 7], 7, 2, 3));
